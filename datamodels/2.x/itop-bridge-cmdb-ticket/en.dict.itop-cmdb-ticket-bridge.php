fcf6edcc66a90d9855c2cce2a868a615b81feb18
Benjamin Planque <benjamin.planque@combodo.com>
2020-07-28 16:51:26 +0200

    move dict from config mgmt

--- datamodels/2.x/itop-bridge-cmdb-ticket/en.dict.itop-cmdb-ticket-bridge.php
+++ datamodels/2.x/itop-bridge-cmdb-ticket/en.dict.itop-cmdb-ticket-bridge.php
@@ -6,7 +6,41 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
+//
+// Class: lnkContactToFunctionalCI
+//
+
 Dict::Add('EN US', 'English', 'English', array(
-	// Dictionary entries go here
+	'Class:lnkContactToFunctionalCI' => 'Link Contact / FunctionalCI',
+	'Class:lnkContactToFunctionalCI+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_id' => 'Functionalci',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_id+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_name' => 'Functionalci name',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_name+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_id' => 'Contact',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_id+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_name' => 'Contact name',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_name+' => '',
 ));
+
+//
+// Class: lnkDocumentToFunctionalCI
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkDocumentToFunctionalCI' => 'Link Document / FunctionalCI',
+	'Class:lnkDocumentToFunctionalCI+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_id' => 'Functionalci',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_id+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_name' => 'Functionalci name',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_name+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_id+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_name' => 'Document name',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_name+' => '',
+));
+
+
+
+
 ?>
