14de2d344bf629cfc0149087382a6b65cabc83c5
BenGrenoble <benjamin.planque@combodo.com>
2020-08-06 16:21:01 +0200

    Dispatch dict between structure and config-mgmt

--- datamodels/2.x/itop-config-mgmt/en.dict.itop-config-mgmt.php
+++ datamodels/2.x/itop-config-mgmt/en.dict.itop-config-mgmt.php
@@ -1503,3 +1503,40 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:Subnet/Tab:FreeIPs-explain' => 'Here is an extract of 10 free IP addresses',
 	'Class:Document:PreviewTab' => 'Preview',
 ));
+
+//
+// Class: lnkContactToFunctionalCI
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkContactToFunctionalCI' => 'Link Contact / FunctionalCI',
+	'Class:lnkContactToFunctionalCI+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_id' => 'Functionalci',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_id+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_name' => 'Functionalci name',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_name+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_id' => 'Contact',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_id+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_name' => 'Contact name',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_name+' => '',
+));
+
+//
+// Class: lnkDocumentToFunctionalCI
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkDocumentToFunctionalCI' => 'Link Document / FunctionalCI',
+	'Class:lnkDocumentToFunctionalCI+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_id' => 'Functionalci',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_id+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_name' => 'Functionalci name',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_name+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_id+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_name' => 'Document name',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_name+' => '',
+));
+
+
+?>
--- datamodels/2.x/itop-config-mgmt/fr.dict.itop-config-mgmt.php
+++ datamodels/2.x/itop-config-mgmt/fr.dict.itop-config-mgmt.php
@@ -1454,3 +1454,94 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'Class:Subnet/Tab:FreeIPs-explain' => 'Voici un échantillon de dix addresses IP disponibles',
 	'Class:Document:PreviewTab' => 'Aperçu',
 ));
+
+
+//
+// Class: lnkDocumentToLicence
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:lnkDocumentToLicence' => 'Lien Document / Licence',
+	'Class:lnkDocumentToLicence+' => '',
+	'Class:lnkDocumentToLicence/Attribute:licence_id' => 'Licence',
+	'Class:lnkDocumentToLicence/Attribute:licence_id+' => '',
+	'Class:lnkDocumentToLicence/Attribute:licence_name' => 'Nom Licence',
+	'Class:lnkDocumentToLicence/Attribute:licence_name+' => '',
+	'Class:lnkDocumentToLicence/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToLicence/Attribute:document_id+' => '',
+	'Class:lnkDocumentToLicence/Attribute:document_name' => 'Nom Document',
+	'Class:lnkDocumentToLicence/Attribute:document_name+' => '',
+));
+
+
+//
+// Class: lnkDocumentToPatch
+//
+
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:lnkDocumentToPatch' => 'Lien Document / Patch',
+	'Class:lnkDocumentToPatch+' => '',
+	'Class:lnkDocumentToPatch/Attribute:patch_id' => 'Patch',
+	'Class:lnkDocumentToPatch/Attribute:patch_id+' => '',
+	'Class:lnkDocumentToPatch/Attribute:patch_name' => 'Nom patch',
+	'Class:lnkDocumentToPatch/Attribute:patch_name+' => '',
+	'Class:lnkDocumentToPatch/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToPatch/Attribute:document_id+' => '',
+	'Class:lnkDocumentToPatch/Attribute:document_name' => 'Nom document',
+	'Class:lnkDocumentToPatch/Attribute:document_name+' => '',
+));
+
+
+//
+// Class: lnkDocumentToSoftware
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:lnkDocumentToSoftware' => 'Lien Document / Logiciel',
+	'Class:lnkDocumentToSoftware+' => '',
+	'Class:lnkDocumentToSoftware/Attribute:software_id' => 'Logiciel',
+	'Class:lnkDocumentToSoftware/Attribute:software_id+' => '',
+	'Class:lnkDocumentToSoftware/Attribute:software_name' => 'Nom logiciel',
+	'Class:lnkDocumentToSoftware/Attribute:software_name+' => '',
+	'Class:lnkDocumentToSoftware/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToSoftware/Attribute:document_id+' => '',
+	'Class:lnkDocumentToSoftware/Attribute:document_name' => 'Nom document',
+	'Class:lnkDocumentToSoftware/Attribute:document_name+' => '',
+));
+
+//
+// Class: lnkContactToFunctionalCI
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:lnkContactToFunctionalCI' => 'Lien Contact / CI Fonctionel',
+	'Class:lnkContactToFunctionalCI+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_id' => 'CI',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_id+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_name' => 'Nom CI',
+	'Class:lnkContactToFunctionalCI/Attribute:functionalci_name+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_id' => 'Contact',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_id+' => '',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_name' => 'Nom contact',
+	'Class:lnkContactToFunctionalCI/Attribute:contact_name+' => '',
+));
+
+//
+// Class: lnkDocumentToFunctionalCI
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:lnkDocumentToFunctionalCI' => 'Lien Document / CI Fonctionel',
+	'Class:lnkDocumentToFunctionalCI+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_id' => 'CI',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_id+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_name' => 'Nom CI',
+	'Class:lnkDocumentToFunctionalCI/Attribute:functionalci_name+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_id+' => '',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_name' => 'Nom Document',
+	'Class:lnkDocumentToFunctionalCI/Attribute:document_name+' => '',
+));
+
+?>
